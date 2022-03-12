9a72207353d2f0333f413a89f84c9aa38bb17693
Brian Marth <marthbrian@gmail.com>
2022-03-12 11:50:54 -0500

    bigInversion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/functionalPerson';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <PersonCard firstName = "Philip" lastName= "Fry" age = {1032} hairColor="orange"/>
+      <PersonCard firstName = "Bender" lastName= "Bot" age = {8} hairColor="steel"/>
+      <PersonCard firstName = "Leela" lastName= "Leeloo" age = {32} hairColor="purple"/>
+      <PersonCard firstName = "Shelly" lastName= "Zoidberg" age = {45} hairColor="chitin"/>
       </header>
     </div>
   );
